f09b246d046ac6bc395f5531b35d931b20f81c59
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-23 22:32:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ function dec(n){
   return n-=1;
 }
 function makeInt(n){
-  if(n>=0){
-    return parseInt(n);
-  }
-  if(n===isNaN){
-    return 'NaN';
-  }
+    return parseInt(n, 10);
 }
 function preserveDecimal(n){
   return parseFloat(n);
